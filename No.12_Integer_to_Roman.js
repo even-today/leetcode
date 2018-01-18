3ce162e33e2d5c31eb21318068baaadcd3711cf9
Elson <lxd_elson@tju.edu.cn>
2018-01-18 18:10:02 +0800

    dev

--- No.12_Integer_to_Roman.js
+++ No.12_Integer_to_Roman.js
@@ -8,6 +8,7 @@ var intToRoman = function(num) {
     let str = "";
     let symbol = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
     let value = [1000,900,500,400, 100, 90,  50, 40, 10, 9,   5,  4, 1];
+
     for (let i = 0; num != 0; ++i) {
         while (num >= value[i])  {
             num -= value[i];
